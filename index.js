a4a851b07be9fe25dd024d0d54970362169b59be
root <root@test.rjft.net>
2015-05-30 00:06:55 +0800

    configure simple server

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    console.log('%s - %s', req.method, req.url);
+  if (req.body) {
+    console.log('----------------------------------');
+    console.log(JSON.stringify(req.body, null, '  '));
+    console.log();
+  } else {
+  }
+  return res.send('\n');
+});
+
+var server = app.listen(process.env.PORT || 8033, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at %s:%s...\n', host, port);
+});
+
