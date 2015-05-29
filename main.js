a8c06efd296329fba136f6d08666950e1fd38bcd
kirisetsz <kirisetsz@gmail.com>
2015-05-30 03:22:04 +0800

    add: dependency debug, lodash

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var debug = require('debug');
+var debug_traffic = debug('debug_traffic');
+
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  console.log('%s - %s', req.method, req.url);
+  debug_traffic('%s - %s', req.method, req.url);
   if (req.body) {
-    console.log('----------------------------------');
-    console.log(JSON.stringify(req.body, null, '  '));
+    debug_traffic('----------------------------------');
+    debug_traffic(JSON.stringify(req.body, null, '  '));
   }
-  console.log();
+  debug_traffic();
   return next();
 });
 app.use('/hooks/github-trello', require('./index'));
