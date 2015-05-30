47673e163b29d28b3a13f51c1da53bedf571c628
kirisetsz <kirisetsz@gmail.com>
2015-05-30 15:12:03 +0800

    refactor...

--- index.js
+++ index.js
@@ -1,77 +1,60 @@
 var _ = require('lodash');
+var async = require('async');
 var express = require('express');
 var router = express.Router();
 
-var analyze = (function (symbols) {
-  var a = _.map(symbols, function (value) { return value.join('|') }).join('|');
-  var h = new RegExp('\\b(' + a + ') #card-([0-9]+)\\b', 'gi');
-  var c = _.transform(symbols, function (object, v, k) {
-    object[k] = new RegExp('^(' + v.join('|') + ')$', 'i');
-  });
-  return function (message) {
-    var match;
-    var last_symbol;
-    var signals = [];
-    while (match = h.exec(message)) {
-      var key = _.findKey(c, function (exp) { return exp.test(match[1]); });
-      if (key == '_') {
-        key = last_symbol;
-      }
-      if (key) {
-        signals.push({ symbol: key, card: match[2] });
-      }
-      last_symbol = key;
-    }
-    return signals;
-  };
-})({
-  'mention': [ 'see', 'refers? to' ],
-  'fulfill': [ 'completes?', 'fulfills?', 'implements?', 'finish(?:es)?' ],
-  '_': [ 'and', ', ?' ],
-});
+var parser = require('./lib/message-parser');
 
 router.use(require('./lib/trello-agent')());
 router.post('/label-card-in-board/:board', function (req, res, next) {
-  var body = req.body;
-  var board = req.params.board;
-  var label_success = req.query.label_success || 'green';
   var trello = res.trello;
-  _.each(body.commits, function (commit) {
-    var actions = analyze(commit.message);
-    var reverts = /^reverts?\b/i.test(commit.message);
-    _.each(actions, function (act) {
-      console.log(act);
-      trello.get('/boards/' + board + '/cards/' + act.card).end(function (err, res) {
+  async.each(
+    _(req.body.commits).map(parser.parse).flatten().value(),
+    function (action, callback) {
+
+      var commit = action.commit, revert = action.revert;
+      var commit_text = '[' + commit.id + '](' + commit.url + ').\n> ' + commit.message.split('\n').join('\n> ');
+
+      return trello.get('/boards/' + req.params.board + '/cards/' + action.cardId).end(function (err, res) {
+        if (err) return callback(err);
+
         var card = res.body;
-        switch (act.symbol) {
+        var labels = _.pluck(card.labels, 'color');
+
+        switch (action.symbol) {
           case 'mention': {
-            trello.post('/cards/' + card.id + '/actions/comments')
-            .send({ text: 'Mentioned by [' + commit.id + '](' + commit.url + ').\n>' + commit.message.split('\n').join('\n>') })
-            .end(function (err, res) { console.log(err, res.body); });
-          } break;
-          case 'fulfill': {
-            var labels = _.pluck(card.labels, 'color');
-            if (!reverts) {
-              trello.post('/cards/' + card.id + '/actions/comments')
-              .send({ text: 'Fulfilled by [' + commit.id + '](' + commit.url + ').\n> ' + commit.message.split('\n').join('\n> ') })
-              .end(function (err) { console.log(err); });
-              trello.put('/cards/' + card.id + '/labels')
-              .send({ value: _.uniq(labels.concat([ label_success ])).join(',') })
-              .end(function (err) { console.log(err); });
-            } else {
-              trello.post('/cards/' + card.id + '/actions/comments')
-              .send({ text: 'Reverted [' + commit.id + '](' + commit.url + ').\n> ' + commit.message.split('\n').join('\n> ') })
-              .end(function (err) { console.log(err); });
-              trello.put('/cards/' + card.id + '/labels')
-              .send({ value: _.filter(labels, function (v) { return v != label_success; }).join(',') })
-              .end(function (err) { console.log(err); });
+            if (!revert) {
+              trello.post('/cards/' + card.id + '/actions/comments').send({ text: 'Mentioned by ' + commit_text }).end(callback);
             }
           } break;
+          case 'fulfill': {
+            if (!revert) {
+              async.parallel({
+                comment: function (callback) {
+                  trello.post('/cards/' + card.id + '/actions/comments').send({ text: 'Fulfilled by ' + commit_text }).end(callback);
+                },
+                label: function (callback) {
+                  trello.put('/cards/' + card.id + '/labels').send({ value: _.union(labels, [ 'green' ]).join(',') }).end(callback);
+                }
+              }, callback);
+            } else {
+              async.parallel({
+                comment: function (callback) {
+                  trello.post('/cards/' + card.id + '/actions/comments').send({ text: 'Reverted ' + commit_text }).end(callback);
+                },
+                label: function (callback) {
+                  trello.put('/cards/' + card.id + '/labels').send({ value: _.difference(labels, [ 'green' ]).join(',') }).end(callback);
+                }
+              }, callback);
+            }
+          } break;
+          default: {
+            callback();
+          } break;
         }
+
       });
-    });
-  });
-  return next();
+    }, next);
 });
 
 module.exports = router;
